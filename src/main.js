2dfd527eaacebfdfeb9c8d8694b78718b621019a
hezhenJia <572286584@qq.com>
2020-02-06 21:29:51 +0800

    导入路由

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+//导入路由
+import router from './router/index.js'
 // 引入
 // 完整引入 在开发阶段
 // 按需引入 项目开发完毕,优化过程
 import ElementUI from 'element-ui'
-import 'elemeng-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 // 使用饿了么
 Vue.use(ElementUI)
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
