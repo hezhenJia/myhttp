f6c294ddf49ba19a33c04b08ad96bc7ff63f651f
hezhenJia <572286584@qq.com>
2020-02-06 20:57:38 +0800

    按钮

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入
+// 完整引入 在开发阶段
+// 按需引入 项目开发完毕,优化过程
+import ElementUI from 'element-ui'
+import 'elemeng-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+// 使用饿了么
+Vue.use(ElementUI)
 
 new Vue({
   render: h => h(App)
