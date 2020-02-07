0c806b588d20f4fdea6132cb730188a39e232e71
hezhenJia <572286584@qq.com>
2020-02-07 20:00:44 +0800

    登录

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,16 @@
 import VueRouter from 'vue-router'
 // 注册
 import Vue from 'vue'
+
+import Login from'@/views/login'
+
 Vue.use(VueRouter)
 
 // 初始化
 const router = new VueRouter({
-  routes:[]
+  routes:[
+    {path: '/login',component:Login}
+  ]
 })
 // 导出
 export default router
---
+++ src/views/login/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container-login">
+    <el-card class="my-card">
+      <img src="../../assets/logo_index.png">
+
+    </el-card>
+
+  </div>
+</template>
+
+<script>
+  export default{
+    name:'app-login'
+  }
+</script>
+
+<style scoped lang="less">
+  .container-login{
+   width: 100%;
+   height: 100%;
+   position: absolute;
+   top: 0;
+   left: 0;
+   background:url(../../assets/login_bg.jpg) ;
+   background-size: cover;
+   .my-card{
+     width: 400px;
+     height: 350px;
+     transform: translate(-50%,-60%);
+     position: absolute;
+     left: 50%;
+     top: 50%;
+     img{
+       width: 200px;
+       display: block;
+       margin: 0 auto 20px;
+       
+     }
+   }
+  }
+</style>
